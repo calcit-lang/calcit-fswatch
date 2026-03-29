f809a6418d7b9c45631ea84de26a41129bd5bce5
tiye <jiyinyiyong@gmail.com>
2026-03-30 00:14:02 +0800

    chore: upgrade cirru_edn 0.7.4, edition 2024, fix API compat; bump to 0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,26 @@
 use std::sync::Arc;
 use std::{collections::HashMap, path::Path};
 
-use cirru_edn::Edn;
+use cirru_edn::{Edn, EdnMapView};
 use notify::event::{DataChange, ModifyKind};
 use notify::{EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn abi_version() -> String {
-  String::from("0.0.6")
+  String::from("0.0.9")
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
+pub fn edn_version() -> String {
+  cirru_edn::version().to_owned()
+}
+
+#[unsafe(no_mangle)]
 pub fn fswatch(
   args: Vec<Edn>,
   handler: Arc<dyn Fn(Vec<Edn>) -> Result<Edn, String> + Send + Sync + 'static>,
-  _finish: Box<dyn FnOnce()>,
+  _finish: Box<dyn FnOnce() + Send + Sync + 'static>,
 ) -> Result<Edn, String> {
   if let Some(options) = args.get(0) {
     match options {
@@ -84,9 +89,9 @@ pub fn fswatch(
 }
 
 fn new_event(t: &str, p: &str, extra: &str) -> Edn {
-  Edn::Map(HashMap::from([
+  Edn::Map(EdnMapView(HashMap::from([
     (Edn::tag("type"), Edn::tag(t)),
     (Edn::tag("path"), Edn::str(p)),
     (Edn::tag("extra"), Edn::str(extra)),
-  ]))
+  ])))
 }
